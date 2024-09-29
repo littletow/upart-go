dc11fe3e17afc914bfe5aba75f1da6642e38105b
eagle <422964429@qq.com>
2024-09-29 11:44:25 +0800

    更新版本号

--- cmd/version.go
+++ cmd/version.go
@@ -10,12 +10,12 @@ func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
-// 当前版本为1.2.0，每次新增功能都增一个版本，bug修复增一个小版本。
+// 每次新增功能都增一个版本，bug修复增一个小版本。
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "获取版本号",
 	Long:  `获取版本号`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v1.3.0")
+		fmt.Println("v2.1.0")
 	},
 }
