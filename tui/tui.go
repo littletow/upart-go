0cf2fc138b4c3b914c6483cf8060d77a493b69d3
eagle <422964429@qq.com>
2024-10-30 10:03:55 +0800

    优化tui

--- tui/tui.go
+++ tui/tui.go
@@ -2,6 +2,7 @@ package tui
 
 import (
 	"fmt"
+	"gart/utils"
 	"math"
 	"strconv"
 	"strings"
@@ -10,7 +11,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/fogleman/ease"
-	"github.com/lucasb-eyer/go-colorful"
 )
 
 const (
@@ -31,7 +31,7 @@ var (
 	mainStyle     = lipgloss.NewStyle().MarginLeft(2)
 
 	// Gradient colors we'll use for the progress bar
-	ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
+	ramp = utils.MakeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
 )
 
 type (
@@ -231,34 +231,6 @@ func progressbar(percent float64) string {
 	return fmt.Sprintf("%s%s %3.0f", fullCells, emptyCells, math.Round(percent*100))
 }
 
-// Utils
-
-// Generate a blend of colors.
-func makeRampStyles(colorA, colorB string, steps float64) (s []lipgloss.Style) {
-	cA, _ := colorful.Hex(colorA)
-	cB, _ := colorful.Hex(colorB)
-
-	for i := 0.0; i < steps; i++ {
-		c := cA.BlendLuv(cB, i/steps)
-		s = append(s, lipgloss.NewStyle().Foreground(lipgloss.Color(colorToHex(c))))
-	}
-	return
-}
-
-// Convert a colorful.Color to a hexadecimal format.
-func colorToHex(c colorful.Color) string {
-	return fmt.Sprintf("#%s%s%s", colorFloatToHex(c.R), colorFloatToHex(c.G), colorFloatToHex(c.B))
-}
-
-// Helper function for converting colors to hex. Assumes a value between 0 and
-// 1.
-func colorFloatToHex(f float64) (s string) {
-	s = strconv.FormatInt(int64(f*255), 16)
-	if len(s) == 1 {
-		s = "0" + s
-	}
-	return
-}
 func Execute() {
 	initialModel := model{0, false, 10, 0, 0, false, false}
 	p := tea.NewProgram(initialModel)
---
+++ utils/style.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+// Utils
+// Generate a blend of colors.
+func MakeRampStyles(colorA, colorB string, steps float64) (s []lipgloss.Style) {
+	cA, _ := colorful.Hex(colorA)
+	cB, _ := colorful.Hex(colorB)
+
+	for i := 0.0; i < steps; i++ {
+		c := cA.BlendLuv(cB, i/steps)
+		s = append(s, lipgloss.NewStyle().Foreground(lipgloss.Color(ColorToHex(c))))
+	}
+	return
+}
+
+// Convert a colorful.Color to a hexadecimal format.
+func ColorToHex(c colorful.Color) string {
+	return fmt.Sprintf("#%s%s%s", colorFloatToHex(c.R), colorFloatToHex(c.G), colorFloatToHex(c.B))
+}
+
+// Helper function for converting colors to hex. Assumes a value between 0 and
+// 1.
+func colorFloatToHex(f float64) (s string) {
+	s = strconv.FormatInt(int64(f*255), 16)
+	if len(s) == 1 {
+		s = "0" + s
+	}
+	return
+}
