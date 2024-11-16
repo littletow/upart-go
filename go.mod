742efff61954a42b7419c649443118ba356e7c6e
eagle <422964429@qq.com>
2024-11-16 15:21:21 +0800

    添加ws

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/charmbracelet/bubbletea v1.1.2
 	github.com/charmbracelet/lipgloss v0.13.0
 	github.com/fogleman/ease v0.0.0-20170301025033-8da417bf1776
+	github.com/gorilla/websocket v1.5.3
 	github.com/hajimehoshi/ebiten/v2 v2.8.4
 	github.com/jedib0t/go-pretty/v6 v6.5.9
 	github.com/lucasb-eyer/go-colorful v1.2.0
--- service/bind.go
+++ service/bind.go
@@ -8,7 +8,9 @@ import (
 	"image/color"
 	"log"
 	"net"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
@@ -68,6 +70,58 @@ func GetMacAddr() string {
 func ConnWs(ctx context.Context, form *BindForm) error {
 	// 获取识别码，并接收绑定结果
 	// 连接Websocket
+	url := fmt.Sprintf("ws://xxx/echo?a=b")
+
+	c, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 	return nil
 }
 
